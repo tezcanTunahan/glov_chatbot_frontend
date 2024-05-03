269ad2e51ac412ba25f59f7c499d92bfcab4fe03
tunahan <ttezcan.1999@gmail.com>
2024-05-03 14:48:07 +0300

    refactor: Handle error in sendMessageService

--- services/chat.ts
+++ services/chat.ts
@@ -6,8 +6,13 @@ type Messages = {
 };
 
 const sendMessageService = async (messages: Messages[], message: string) => {
-  const res = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/chatbot/chat', { messages: [...messages, { role: 'user', content: message }] });
-  return Promise.resolve(res);
+  try {
+    const res = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/chatbot/chat', { messages: [...messages, { role: 'user', content: message }] });
+    return Promise.resolve(res);
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
 };
 
 export { sendMessageService };
